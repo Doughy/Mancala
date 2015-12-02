522e66f455dce869f7e11576ad9a63ddf987f03e
GSU <garysu413@gmail.com>
2015-12-02 12:41:43 -0800

    Marshall Changes

--- src/GUITester.java
+++ src/GUITester.java
@@ -2,9 +2,8 @@ import javax.swing.JFrame;
 
 
 public class GUITester {
-	public static String[] layouts={"NormalLayout","myBlackandWhiteLayout"};
 	public static void main(String[] args) {
-		startBox startBox = new startBox(layouts);
+		startBox startBox = new startBox();
 		startBox.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		startBox.showStartUpBox();
 	}
